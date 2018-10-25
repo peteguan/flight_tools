8378b0fb011629365dbfb5a0ccf1c5ec40138834
peteguan <pete.g.epic@outlook.com>
2018-10-24 22:56:30 -0700

    dev

--- report_graphic/views.py
+++ report_graphic/views.py
@@ -1,3 +1,36 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
 from django.shortcuts import render
+from pyecharts import GeoLines, Style
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+style = Style(
+    title_top="#fff",
+    title_pos="center",
+    width=1600,
+    height=800,
+    background_color="#58FAF4",
+)
+
+style_geo = style.add(
+    is_label_show=True,
+    line_curve=0,
+    line_opacity=0.6,
+    legend_text_color="#ff",
+    legend_pos="right",
+    label_color=['#910000', '#ffa022', '#46bee9'],
+    label_pos="right",
+    label_formatter="{b}",
+    label_text_color="#",
+    symbol=[None,None],
+    maptype=u"中国",
+    is_geo_effect_show=False,
+    geo_normal_color="#BDD3C6"
+    # legend_selectedmode="single"
+)
+
